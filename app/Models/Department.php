bf6d9e1e994fd587f4568cdbabbbe7ecc8ec7183
Mohamad Amir Dliwati <mohamad.amir.dliwati@gmail.com>
2024-02-16 19:31:41 +0300

    Init CMS

---
+++ app/Models/Department.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+    use HasFactory;
+	public $table="departments";
+
+    public function branch()
+    {
+        return $this->belongsTo('App\Models\Branch','branch_id','id');
+    }
+
+    public function positions()
+    {
+        return $this->hasMany('App\Models\Position','department_id','id');
+    }
+
+}
---
+++ app/Models/Position.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Position extends Model
+{
+    use HasFactory;
+	public $table="positions";
+
+	public function department()
+    {
+        return $this->belongsTo('App\Models\Department','department_id','id');
+    }
+
+    public function employees()
+    {
+        return $this->hasMany('App\Models\Employee','position_id','id');
+    }
+}
